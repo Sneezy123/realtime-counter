ec7f4ccf2ee326f22884a24074be6b545e7134fe
Nils Müller <nilshendrik13@gmail.com>
2026-05-05 20:43:56 +0200

    fix 404 bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  base: "/",
+  assetsInclude: ["**/*.html"],
+  plugins: [react(), tailwindcss()],
   // Removed exclude for lucide-react to fix module loading in Firefox
   optimizeDeps: {
     // exclude: ['lucide-react'],
